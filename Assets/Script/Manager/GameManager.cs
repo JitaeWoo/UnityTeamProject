8f88aaae22c4b5cb43ff444d0a6175182dc05b6b
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-04-28 16:24:33 +0900

    [Feat] GameManager에 씬 전환 기능 추가
    각 씬의 전환을 위해 GameManager에 SceneChange 메서드 추가

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -11,10 +12,16 @@ public class GameManager : Singleton<GameManager>
     public void StartGame()
     {
         OnStartGame?.Invoke();
+        SceneChange("Stage1");
     }
 
     public void GameOver()
     {
         OnGameOver?.Invoke();
+    }
+
+    public void SceneChange(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
