7d0ef3dea6a5b6e3a2170483d81328a7d74fef11
devhoon1999 <devhoon1999@gmail.com>
2025-04-28 14:54:38 +0900

    [Feat] UI 매니저 구현
    UI 동작 제어하는 싱글톤 스크립트 추가

---
+++ Assets/Script/Manager/Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager 
+{
+    public GameManager Game => GameManager.GetInctance();
+    public PlayerManager Player => PlayerManager.GetInctance();
+    public StageManager Stage => StageManager.GetInctance();
+
+    public UIManager UI => UIManager.GetInctance();
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initailize()
+    {
+        GameManager.CreateInstance();
+        PlayerManager.CreateInstance();
+        StageManager.CreateInstance();
+        UIManager.CreateInstance();
+    }
+}
