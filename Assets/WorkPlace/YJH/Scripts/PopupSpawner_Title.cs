c21575410ea0db1e8d2a6564fea16acf65a30e16
devhoon1999 <devhoon1999@gmail.com>
2025-04-30 21:31:30 +0900

    [Feat] 조작법 팝업 생성 및 파괴 구현
    타이틀 씬의 Control Manual Button 과 팝업창 프리팹의 CloseButton_Manual 의 x 버튼의 온클릭 이벤트를 활용해서 팝업창의 생성과 파괴를 구현함.

--- Assets/WorkPlace/YJH/Scripts/PopupSpawner_Title.cs
+++ Assets/WorkPlace/YJH/Scripts/PopupSpawner_Title.cs
@@ -1,34 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PopupSpawner : MonoBehaviour
 {
-    public GameObject popupPrefab;  // 팝업 프리팹
-    private GameObject currentPopup;  // 현재 활성화된 팝업
+    public GameObject prefabToSpawn; // 생성할 프리팹
+    public Transform parentTransform; // 생성될 부모
 
-    // 팝업을 생성하는 메서드
-    public void SpawnPopup()
+    public void SpawnPrefab()
     {
-        // 팝업 생성
-        currentPopup = Instantiate(popupPrefab, transform.position, Quaternion.identity);
-
-        // 팝업의 X 버튼을 찾아서 클릭 시 팝업을 삭제하도록 설정
-        Button closeButton = currentPopup.transform.Find("CloseButton_Title").GetComponent<Button>();
-        if (closeButton != null)
-        {
-            closeButton.onClick.AddListener(ClosePopup);
-        }
-    }
-
-    // 팝업을 삭제하는 메서드
-    public void ClosePopup()
-    {
-        if (currentPopup != null)
-        {
-            Destroy(currentPopup);  // 팝업을 삭제
-        }
+        Instantiate(prefabToSpawn, parentTransform);
     }
 }
-
