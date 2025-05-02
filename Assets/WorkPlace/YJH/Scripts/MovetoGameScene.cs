9972af0c50757fca06aa0609961981ff567c4873
Ji Hoon Yeo <devhoon1999@gmail.com>
2025-05-02 15:43:18 +0900

    [Rename] 씬, 스크립트 이름 수정
    구분하기 쉽게 씬과 스크립트의 이름을 수정함.

--- Assets/WorkPlace/YJH/Scripts/MovetoGameScene.cs
+++ Assets/WorkPlace/YJH/Scripts/MovetoGameScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
+public class MovetoGameScene : MonoBehaviour
 {
     public void GoToGameScene()
     {
