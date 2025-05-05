7fb19cb5182d441b80d4055ca9a3ea806f05439a
eunjegolem <ppoppos@hotmail.com>
2025-05-05 13:55:12 +0900

    [Feat]보스몬스터 생성 코드 추가, 맵생성 스크립트 추가
    보스몬스터 생성 코드 추가됨
    맵 생성 스크립트 추가됨

---
+++ Assets/WorkPlace/LJH/MapCreator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject groundPrefab;
+    public GameObject wallPrefab;
+    public int width = 100;
+    public int height = 100;
+    public int mapStartXPos = -101;
+    public int mapStartZPos = -101;
+    public int FloorPos = -1;
+
+    void Awake()
+    {
+        for (int x = mapStartXPos; x <= width+1; x++)
+        {
+            for (int z = mapStartZPos; z <= height+1; z++)
+            {
+                Vector3 groundpos = new Vector3(x, FloorPos, z);
+                Instantiate(groundPrefab, groundpos, Quaternion.identity, this.transform);
+                for (int y = 0; y < 4; y++)
+                {
+                    if (x== mapStartXPos || x== width+1||z== mapStartZPos || z== height+1) 
+                    { 
+                        Vector3 wallpos = new Vector3(x, y, z);
+                        Instantiate(wallPrefab, wallpos, Quaternion.identity, this.transform);
+                    }
+                }
+            }
+        }
+    }
+}
